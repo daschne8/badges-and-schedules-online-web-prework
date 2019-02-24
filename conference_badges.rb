5ee465f505f87196e2da20b026c3677c84d47dfc
Daniel Schneider <daschne8@gmail.com>
2019-02-24 21:34:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges.push(badge_maker(name))
+  end
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  for room_num in range 1..7 do
+    speaker = speakers[room_num-1]
+    room_assignments.push("Hello, #{speaker}! You'll be assigned to room #{room_num}!")
+  end
+  return room_assignments
+end
